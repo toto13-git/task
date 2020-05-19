6e83c7e5f3ecab5676274402f38cd66c175524f4
toto13-git <56705907+toto13-git@users.noreply.github.com>
2020-05-19 17:26:36 +0900

    本番環境(Heroku)でhttpsを強制する

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
     //
     Schema::defaultStringLength(191);
 
+    // 本番環境(Heroku)でhttpsを強制する
     if (\App::environment('production')) {
       \URL::forceScheme('https');
     }
