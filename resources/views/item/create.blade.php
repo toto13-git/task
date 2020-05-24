5a7f1664ef0a60511a2b790ece814ccfdb7b0294
toto13-git <56705907+toto13-git@users.noreply.github.com>
2020-05-25 01:08:38 +0900

    view調整

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -5,7 +5,7 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">Dashboard</div>
+        <div class="card-header">備蓄品の登録</div>
 
         <div class="card-body">
           @if (session('status'))
